ef311dbfd595c8a2c5b16b80ff0b6123c80f1515
fjkun06 <fjkun06@gmail.com>
2025-09-14 22:08:03 +0200

    handling file errors

---
+++ error_handling.py
@@ -0,0 +1,27 @@
+# try:
+#     print(5 / 0)
+# except ZeroDivisionError:
+#     print("You can't divide a number by 0")
+
+
+def divider():
+    """Sample divider"""
+    print("Give me two numbers, and I'll divide them.")
+    print("Enter 'q' to quit.")
+
+    while True:
+        first_number = input("\nFirst number: ")
+        if first_number == "q":
+            break
+        second_number = input("Second number: ")
+        if second_number == "q":
+            break
+
+        try:
+            answer = int(first_number) / int(second_number)
+        except ZeroDivisionError:
+            print("You can't divide a number by 0")
+        else:
+            print(answer)
+            
+divider()
