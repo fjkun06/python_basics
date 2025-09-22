cecb395fc194a88c0280bb63a26118325045c094
fjkun06 <fjkun06@gmail.com>
2025-09-22 13:53:13 +0200

    ...

--- data analysis/mpl_squares.py
+++ data analysis/mpl_squares.py
@@ -1,17 +1,20 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+input_values = range(1, 1001)
+squares = [value**2 for value in input_values]
 
 plt.style.use("seaborn-v0_8-darkgrid")
 fig, ax = plt.subplots()
 
-ax.plot(input_values, squares, linewidth=1)
+# ax.plot(input_values, squares, linewidth=.1)
+ax.scatter(input_values, squares, c=squares, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and axis labels
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
+ax.axis([0, 1100, 0, 1_100_000])
+ax.ticklabel_format(style='scientific')
 
 # Set size of tick labels
 ax.tick_params(labelsize=14)
